f99c29feddeaa4a898fdec9cbce6244e9eaab004
Patrick Luan de Mattos <patrick.mattos@pampa.compasso>
2020-10-27 15:36:36 -0300

    inserindo api ao projeto e fazendo primeiro request

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App';
+  //TypeScript = Quando não tipamos o dado, o mesmo retorn tipo n
+   photos:Object[] = [];
+
+  constructor(http: HttpClient) {
+
+    //Precisamos tipar o dado, pois o Angular não sabe o tipo que estará vindo do Back
+    http.get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe(
+        photos => this.photos = photos,
+            err => console.log(err.message)
+      );
+  }
+
 }
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -3,6 +3,8 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PhotosModule} from "./photos/photos.module";
+//Nossa classe de Http, lembrando que a mesma se repete no componente
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   //Apenas components
@@ -14,7 +16,9 @@ import {PhotosModule} from "./photos/photos.module";
     //BrowserModule importa automaticamente quando importamos um module
     BrowserModule,
     AppRoutingModule,
-    PhotosModule
+    PhotosModule,
+    //Nossa classe de Http, lembrando que a mesma se repete no componente
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

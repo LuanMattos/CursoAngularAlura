95a2ec6fb1053994915f3778d9af0614ca7b1f0d
luanmattos <patrickluan.matos@gmail.com>
2020-10-28 20:12:08 -0300

    criado próprio componente para mensagem de erro do form

--- App/src/app/home/home.module.ts
+++ App/src/app/home/home.module.ts
@@ -2,17 +2,19 @@ import {NgModule} from "@angular/core";
 import {SignInComponent} from "./signin/signin.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import {VmessageModule} from "../shared/vmessage/vmessage.module";
 
 /**
  * Como ele vai ser carregado pelo app.module e está a nível de página, não precisamos usar Exports
  * **/
 @NgModule({
   declarations:[
-    SignInComponent
+    SignInComponent,
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VmessageModule
   ]
 })
 export class HomeModule{}

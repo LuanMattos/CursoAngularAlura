cb86e7d965056bd2b6a70e54992af02adf975693
luanmattos <patrickluan.matos@gmail.com>
2020-10-28 11:53:40 -0300

    Desmembrando os modules

--- App/src/app/photos/photos.module.ts
+++ App/src/app/photos/photos.module.ts
@@ -1,35 +1,27 @@
 /** Aqui criamos um Feature, para não encher o app.module de declarações a medida que o App cresce **/
 import {NgModule, Pipe} from "@angular/core";
-import {CommonModule} from "@angular/common";
 /**
  * Nossa classe de Http, lembrando que a mesma se repete no componente,
  *  vamos instancia-la apenas nos Module dos Services, por questões de performance e
  *  porque vamos precisar dela apenas nos Modules dos Services mesmo
  **/
-import {HttpClientModule} from "@angular/common/http";
-import {PhotoComponent} from "./photo/photo.component";
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import {FilterByDescription} from "./photo-list/filter-by-description.pipe";
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import {PhotoListModule} from "./photo-list/photo-list.module";
+import {PhotoFormModule} from "./photo-form/photo-form.module";
+import {PhotoModule} from "./photo/photo.module";
 
 @NgModule({
-  /** Como no C#, o declaration representaria um Private **/
-  declarations:[
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescription,
-    LoadButtonComponent
-  ],
+  /**
+   * Como no C#, o declaration representaria um Private, ou seja
+   *  se eu declaro uma biblioteca neste módulo, o mesmo NÃO instancia para seus filhos
+   * Perceba que em TODOS os módulos FILHOS precisamos importar o CommonModule
+   **/
+  declarations:[],
   /** Sem o exports, o Module não funciona **/
   imports:[
-    HttpClientModule,
-    /** Lembra da "treta" do BrowserModule? (explicação em app.module)*/
-    CommonModule
-  ]
+    PhotoListModule,
+    PhotoFormModule,
+    PhotoModule
+  ],
 })
 export class PhotosModule{
 

e4195352ad6e6d8c8e77563ceb41f0295939c007
luanmattos <patrickluan.matos@gmail.com>
2020-10-28 20:55:24 -0300

    ViewChild e validacao da tela de login

--- App/src/app/home/home.module.ts
+++ App/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import {SignInComponent} from "./signin/signin.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {VmessageModule} from "../shared/vmessage/vmessage.module";
+import {RouterModule} from "@angular/router";
 
 /**
  * Como ele vai ser carregado pelo app.module e está a nível de página, não precisamos usar Exports
@@ -14,7 +15,13 @@ import {VmessageModule} from "../shared/vmessage/vmessage.module";
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    VmessageModule
+    VmessageModule,
+    /**
+     * No app.routing.module, já temos o mesmo importado, e deve funcionar em todos os filhos
+     * pois estão dentro (juntos) do import.
+     * Porém ainda é uma boa prática colocar ele no home.module pra gente saber que home.module depende do mesmo
+     * **/
+    RouterModule
   ]
 })
 export class HomeModule{}
--- App/src/app/home/signin/signin.component.ts
+++ App/src/app/home/signin/signin.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {Router} from "@angular/router";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../core/auth.service";
 
 @Component({
   templateUrl:'./signin.component.html'
@@ -9,14 +11,36 @@ export class SignInComponent implements OnInit{
   /** Se liga com o Form através do data Bind [formGroup] */
   loginForm:FormGroup;
 
+  /** Pega o elemento do html ** OU ElementRef<HTMLInputElement> **/
+  @ViewChild('userNameInput') userNameInput:ElementRef;
+
   /** Construtor de Formulário **/
-  constructor( private formBuilder:FormBuilder ) {}
+  constructor(
+    private formBuilder:FormBuilder,
+    private authService:AuthService,
+    private router:Router
+    ) {}
 
   ngOnInit():void{
     this.loginForm = this.formBuilder.group({
         userName:['',Validators.required],
         password:['',[Validators.required]]
     });
+  }
+  login(){
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService.authenticate(userName,password)
+      .subscribe(
+        ()=>this.router.navigate(['user',userName]),
+        err=> {
+          console.log(err.message)
+          /** Reza a lenda que não podemos usar Rendered, apenas o nativo do Angular **/
+          this.userNameInput.nativeElement.focus();
+          this.loginForm.reset();
+        }
+      )
   }
 
 }

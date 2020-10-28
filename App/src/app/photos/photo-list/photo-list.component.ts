ab2a8f08d8d5703b3dbcd5e463c4bba0017e5478
luanmattos <patrickluan.matos@gmail.com>
2020-10-28 10:00:32 -0300

    pipe com Observable, melhorando performance da barra de busca

--- App/src/app/photos/photo-list/photo-list.component.ts
+++ App/src/app/photos/photo-list/photo-list.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Subject} from "rxjs";
+import {debounceTime} from 'rxjs/operators';
+
 import {Photo} from '../photo/photo';
 import {PhotoService} from '../photo/photo.service';
-import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit,OnDestroy {
 
   title = 'App';
   /** TypeScript = Quando não tipamos o dado, o mesmo retorn tipo n **/
   photos:Photo[] = [];
   filter:string = '';
+
+  /**
+   * Contrário do Observable que é multicast, o Subject é multicast e envia para todos assinantes
+   * https://rxjs-dev.firebaseapp.com/guide/subject
+   * https://tegra.com.br/manipulando-estado-com-observables-e-subjects-usando-rxjs/
+   * **/
+  debounce:Subject<string> = new Subject<string>();
 
   /**
    * (inserido no resolver)
@@ -37,8 +47,15 @@ export class PhotoListComponent implements OnInit {
     // this.photoService.listFromUser(userName)
     //   .subscribe(photos => this.photos = photos)
 
-    /** Busca os dados do activateRoute **/
+    /** Busca os dados do activateRoute, ou seja, a requisição será feita por ROTA **/
     this.photos = this.activatedRoute.snapshot.data['photos'];
-
+    this.debounce
+      /** Mágica que faz captar a ultima sentença digitada após 400ms **/
+      .pipe(debounceTime(400))
+      .subscribe(filter => this.filter = filter);
+  }
+  ngOnDestroy():void {
+    /** Precisamos desalocar memória do nosso Observable **/
+    this.debounce.unsubscribe()
   }
 }

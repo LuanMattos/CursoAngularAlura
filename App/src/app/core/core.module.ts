53b1e15d99b50b637b2108f4c42a46ce4c867465
luanmattos <patrickluan.matos@gmail.com>
2020-10-29 14:42:07 -0300

    routerLink

--- App/src/app/core/core.module.ts
+++ App/src/app/core/core.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from "@angular/core";
-import {HeaderComponent} from "./header/header.component";
 import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations:[HeaderComponent],
-  imports:[CommonModule],
+  imports:[
+    CommonModule,
+    RouterModule
+  ],
   exports:[HeaderComponent]
 })
 export class CoreModule{}

d21bc3155bae2bc28ebe49348a2dedc03b1dcd0b
luanmattos <patrickluan.matos@gmail.com>
2020-10-30 11:55:24 -0300

    Requeset Interceptor

--- App/src/app/core/core.module.ts
+++ App/src/app/core/core.module.ts
@@ -3,6 +3,8 @@ import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 
 import {HeaderComponent} from "./header/header.component";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {RequestInterceptor} from "./auth/request.interceptor";
 
 @NgModule({
   declarations:[HeaderComponent],
@@ -10,6 +12,14 @@ import {HeaderComponent} from "./header/header.component";
     CommonModule,
     RouterModule
   ],
-  exports:[HeaderComponent]
+  exports:[HeaderComponent],
+  /** Apartir de agora, o Angular 'não manda mais nas rotas' e sim nosso request.interceptor **/
+  providers:[
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:RequestInterceptor,
+      multi:true
+    }
+  ]
 })
 export class CoreModule{}
